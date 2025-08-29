77a45f1ded4cd5d8aa999fb5de5717f544686790
양재현 <sheep@yangjaehyeon-ui-MacBookAir.local>
2025-08-29 11:46:20 +0900

    초기세팅

---
+++ MyDays/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  MyDays
+//
+//  Created by 양재현 on 8/29/25.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        // 앱 시작 시 초기 설정
+//        print("앱 시작!")
+        return true
+    }
+    
+}
---
+++ MyDays/App/MyDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDaysApp.swift
+//  MyDays
+//
+//  Created by 양재현 on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MyDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
