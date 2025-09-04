1bddd9a4467dc9ff2848bf025d6382abe71c90bd
양재현 <sheep@yangjaehyeon-ui-MacBookAir.local>
2025-09-04 21:51:42 +0900

    feat: 닉네임 페이지 수정

--- MyDays/Features/SetNickname/View/SetNicknameView.swift
+++ MyDays/Features/SetNickname/View/SetNicknameView.swift
@@ -16,39 +16,70 @@ struct SetNicknameView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             Text("마이데이즈에서 사용할\n닉네임을 적어주세요")
+                .font(.t2())
                 .foregroundColor(.white)
-                .padding(.top, 100)
+                .padding(.top, 83)
             
-            TextField("닉네임을 입력해주세요", text: $vm.nickName)
-                .padding()
-                .overlay(alignment: .bottom) {
-                  Divider()
-                        .frame(height: 1)
-                        .overlay(.white)
+ 
+            ZStack(alignment: .leading) {
+                //placeholder
+                if vm.nickName.isEmpty {
+                    Text("닉네임을 입력해주세요")
+                        .font(.b2())
+                        .foregroundColor(.mdDim)
+                        .padding(.leading, 10)
                 }
-                .padding(.top, 100)
-            
-            //TODO: 정규식
-            Button(action: { vm.setNickname() }) { //TODO: isLoading이면 프로그레스바?
-                Text("닉네임 설정 완료 버튼")
+                
+                //텍스트 필드
+                TextField("", text: $vm.nickName)
+                    .accentColor(.white)
+                    .font(.b2())
                     .foregroundColor(.white)
+                    .padding(.leading, 10)
+                    .padding(.vertical, 16.5)
+                    .overlay(alignment: .bottom) {
+                        Divider()
+                            .frame(height: 1)
+                            .overlay(.white)
+                    }
+                    .onChange(of: vm.nickName) { _, newValue in
+                        // 입력할 때마다 유효성 검사
+                        if !newValue.isEmpty {
+                            vm.isNicknameValid = vm.isNicknameValid(newValue)
+                        }
+                    }
+                
+                
             }
-            .disabled(vm.isDisabled)
-            .frame(maxWidth: .infinity)
-            .padding(.top, 30)
+            .padding(.top, 104)
+            
+            //닉네임 형식 올바르지 않을때 문구
+            if !vm.isNicknameValid {
+                Text("닉네임은 1~12자, 한글, 영어, 숫자만 가능합니다.")
+                    .foregroundColor(Color(hex: "FF5454"))
+                    .font(.b3())
+                    .padding(.top, 12)
+                    .padding(.leading, 19)
+            }
             
             Spacer()
-           
         }
+        .toolbar(.hidden, for: .navigationBar)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .overlay(alignment: .top) {
             Image("logo.set.nickname")
         }
-        .overlay(alignment: .bottom) {
-            Image("mydays.splash")
-        }
+        
         .padding(.horizontal, 30)
         .background(.mdSurf2)
+        //다음 플로팅 버튼
+        .safeAreaInset(edge: .bottom) {
+            Button("다음") { vm.setNickname() }
+                .buttonStyle(.primary(isDisabled: vm.isDisabled))
+                .padding(.horizontal, 30)
+                .padding(.bottom, 10)
+                .disabled(vm.isDisabled)
+        }
         //닉네임 설정 성공 시 감지해서 메인 화면으로 전환
         .onChange(of: vm.isSwitchMain) { _, newValue in
             if newValue {
--- MyDays/Features/SetNickname/ViewModel/SetNicknameViewModel.swift
+++ MyDays/Features/SetNickname/ViewModel/SetNicknameViewModel.swift
@@ -13,9 +13,10 @@ class SetNicknameViewModel: ObservableObject {
     @Published var isSwitchMain: Bool = false //홈 화면으로 전환할지 (닉네임 설정 성공시)
     @Published var nickName: String = ""
     @Published var isLoading: Bool = false //닉넴 설정 로딩
+    @Published var isNicknameValid: Bool = true //닉네임 유효 여부
     
     var isDisabled: Bool { //닉네임 버튼 비활성화 여부
-        nickName.isEmpty
+        nickName.isEmpty || !isNicknameValid(nickName)
     }
     
     private let setNicknameService = MockSetNicknameService() //의존성 주입 (Real or Mock)
@@ -27,12 +28,21 @@ class SetNicknameViewModel: ObservableObject {
             do {
                 _ = try await setNicknameService.setNickname(request: PostNicknameRequest(nickName: nickName))
                 
-                self.isLoading = false
+              
                 self.isSwitchMain = true
             }
             catch {
-                
+                //TODO: 닉네임 중복 알럿 띄워야함...(service에서 ?)
             }
         }
+        self.isLoading = false
     }
+    
+    // MARK: - 닉네임 유효성 검증
+    func isNicknameValid(_ nickname: String) -> Bool {
+        // 가-힣: 완성형 한글
+        let regex = "^[가-힣a-zA-Z0-9]{1,12}$"
+        return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: nickname)
+    }
+    
 }
