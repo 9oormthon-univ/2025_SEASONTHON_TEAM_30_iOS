3b2fbd27534b04a5bbb96eeaa8426c0b7b084374
Apple <apple@Appleui-MacBookPro.local>
2025-09-07 05:54:46 +0900

    feat: 리팩토링

--- MyDays/Features/ChallengeStatus/Service/ChallengeStatusServerModel.swift
+++ MyDays/Features/ChallengeStatus/Service/ChallengeStatusServerModel.swift
@@ -5,7 +5,7 @@
 //  Created by Apple on 9/5/25.
 //
 
-//MARK: - 챌린지 상태 뷰 서버모델
+//MARK: - 챌린지 현황 뷰 서버모델
 import Foundation
 
 // MARK: - GetChallengeStatusResponse
@@ -21,28 +21,6 @@ struct GetChallengeStatusResponse: Decodable {
     let daysCount: Int
     let isCompleteMission: Bool
     
-    /*
-    let meta: Meta
-    let body: ChallengeStatusBody
-    
-    struct Meta: Decodable {
-        let code: Int
-        let message: String
-    }
-    
-    struct ChallengeStatusBody: Decodable {
-        let nickName: String
-        let growthMessage: String
-        let isBubbleVisible: Bool
-        let userTitle: String
-        let userTitleColor: String
-        let progress: Double
-        let imageUrl: String
-        let totalChallengeCount: Int
-        let daysCount: Int
-        let isCompleteMission: Bool
-    }
-     */
 }
 
 // MARK: - Request / Response 모델 정의
@@ -62,31 +40,12 @@ struct EmptyResponse: Decodable {}
 
 // MARK: - User Titles Response
 struct GetUserTitlesResponse: Decodable {
-    
     let titles: [Title]
     struct Title: Decodable {
         let titleId: String
         let title: String
         let titleColor: String
     }
-
-    /*
-    struct Meta: Decodable {
-        let code: Int
-        let message: String
-    }
-    
-    struct Title: Decodable {
-        
-    }
-    
-    struct Body: Decodable {
-        let titles: [UserTitle]
-    }
-    
-    let meta: Meta
-    let body: Body
-     */
 }
 
 
--- MyDays/Features/ChallengeStatus/Service/ChallengeStatusService.swift
+++ MyDays/Features/ChallengeStatus/Service/ChallengeStatusService.swift
@@ -10,7 +10,6 @@ import Alamofire
 import Foundation
 
 // MARK: - ChallengeStatusService Protocol
-// 실제 서비스와 Mock 서비스 모두 이 프로토콜을 따릅니다.
 protocol ChallengeStatusServiceProtocol {
     func getChallengeStatus() async throws -> ChallengeStatusComponent
     func getUserTitles() async throws -> [TitleComponent.Title]
@@ -26,7 +25,7 @@ class ChallengeStatusService: ChallengeStatusServiceProtocol {
             return ChallengeStatusComponent(from: response)
     }
     
-    // 보유 칭호 조회
+    // 보유 칭호 조회 api
     func getUserTitles() async throws -> [TitleComponent.Title]{
         let response: GetUserTitlesResponse = try await APIManager.shared.request(
                 "/users/titles",
@@ -39,11 +38,10 @@ class ChallengeStatusService: ChallengeStatusServiceProtocol {
 
     }
     
-    // 대표 칭호 변경
+    // 대표 칭호 변경 api
     func updateActiveTitle(titleId: String) async throws {
         let requestBody = UpdateActiveTitleRequest(titleId: titleId)
             
-        // 반환 값이 없을 경우의 요청
         _ = try await APIManager.shared.request(
                 "/users/active-title",   // 명세서에 나온 endpoint
                 method: .put,
@@ -58,9 +56,9 @@ class ChallengeStatusService: ChallengeStatusServiceProtocol {
 
 //MARK: - MockChallengeStatusService (테스트용)
 class MockChallengeStatusService: ChallengeStatusServiceProtocol {
-    // 챌린지 상태 뷰 조회 api
+
     func getChallengeStatus() async throws -> ChallengeStatusComponent {
-        return ChallengeStatusComponent.mock // <-- 저희가 모델에서 만든 가짜 데이터에요
+        return ChallengeStatusComponent.mock
     }
     func getUserTitles() async throws -> [TitleComponent.Title] {
             return [
